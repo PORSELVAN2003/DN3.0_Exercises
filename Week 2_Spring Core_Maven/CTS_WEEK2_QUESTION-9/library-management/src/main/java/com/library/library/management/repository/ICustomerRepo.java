5f68653e7c88022079ce4db3bcd0928c6e1f730b
PORSELVAN2003 <111323524+PORSELVAN2003@users.noreply.github.com>
2024-08-08 18:09:36 +0530

    Delete Week 2_Spring Core_Maven directory

--- Week 2_Spring Core_Maven/CTS_WEEK2_QUESTION-9/library-management/src/main/java/com/library/library/management/repository/ICustomerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.library.library.management.repository;
-import com.library.library.management.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICustomerRepo extends JpaRepository<Customer,Long> {
-}
